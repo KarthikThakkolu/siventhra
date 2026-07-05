ec8e287b88e3db59f47c27b75c81813ab06a1c35
KarthikThakkolu <150405840+KarthikThakkolu@users.noreply.github.com>
2026-07-05 13:48:11 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
-import Navbar from './Components/Navbar';
+import "./App.css";
+
+import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Homepage from "./Main/Homepage";
 import About from "./Main/About";
@@ -12,13 +13,17 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
+
         <main>
           <Routes>
-            <Route path="/Homepage" element={<Homepage/>}/>
-            <Route path="/About" element={<About/>}/>
+            {/* Default Home Page */}
+            <Route path="/" element={<Homepage />} />
 
+            {/* About Page */}
+            <Route path="/about" element={<About />} />
           </Routes>
         </main>
+
         <Footer />
       </Router>
     </div>
