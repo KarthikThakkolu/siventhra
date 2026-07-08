d00ebd2548ab759abc3fc0ee7c0e23243b9c216a
KarthikThakkolu <150405840+KarthikThakkolu@users.noreply.github.com>
2026-07-08 21:18:45 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/orderrequest" element={<OrderRequest />} />
-        <Route path="/Feedback" element={<Feedback />} />
+        {/* <Route path="/orderrequest" element={<OrderRequest />} /> */}
+        {/* <Route path="/Feedback" element={<Feedback />} /> */}
         {/* <Route path="/Pages/Curtains" element={<Curtains />} /> */}
       </Routes>
 
